296daa18fad14b7aa8d0da59efacf98649bfd79b
Ruben Ellefsen <ellefsen88@gmail.com>
2023-11-27 13:06:11 +0100

    testing

--- examify/src/App.jsx
+++ examify/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Login from './components/Login.jsx'
+
 
 function App() {
   
   return (
     <>
       <h1 className='text-red-500 text-xl font-bold'>Hello TailwindCss</h1>
+      <Login />
     </>
   )
 }
