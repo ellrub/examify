2d490ec3c7f0591bd3722c93065b02505fdf2a7a
Ruben Ellefsen <ellefsen88@gmail.com>
2023-12-07 21:18:05 +0100

    add keyboard shortcuts and styling

--- examify/src/components/Exam.jsx
+++ examify/src/components/Exam.jsx
@@ -66,7 +66,43 @@ function Exam({ setUserAnswers }) {
       setShuffledExams(shuffledExams);
     });
   }, [examId]);
-
+  
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      switch (event.key) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+          if (currentExamData) {
+            handleAnswer(currentExamData.options[parseInt(event.key) - 1]);
+          }
+          break;
+        case 'Enter':
+          if (answers[currentQuestion] !== null) {
+            nextQuestion();
+          }
+          break;
+        case 'ArrowRight':
+          if (answers[currentQuestion] !== null) {
+            nextQuestion();
+          }
+          break;
+        case 'ArrowLeft':
+          prevQuestion();
+          break;
+        default:
+          break;
+      }
+    };
+  
+    window.addEventListener('keydown', handleKeyDown);
+  
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [currentQuestion, shuffledExams, answers]);
+  
   const currentExamData = shuffledExams[currentQuestion];
 
   if (!currentExamData) {
@@ -87,21 +123,43 @@ function Exam({ setUserAnswers }) {
             <div 
               key={index} 
               onClick={() => handleAnswer(option)}
-              className={`p-3 rounded-lg cursor-pointer text-xl border border-indigo-500 ${answers[currentQuestion]?.answer === option ? 'bg-indigo-700' : 'bg-indigo-900 hover:bg-indigo-700'}`}
+              className={`group relative flex items-center p-4 rounded-lg cursor-pointer text-xl border-2 border-indigo-500 ${answers[currentQuestion]?.answer === option ? 'border-2 border-indigo-300 bg-indigo-700' : 'bg-indigo-900 hover:bg-indigo-700'}`}
             >
+              <span className="box_shadow hidden dp:flex mr-2 bg-indigo-500 text-indigo-50 rounded w-6 h-6 flex items-center justify-center">{index + 1}</span>
               <p className="ml-2">{option}</p>
+              {window.innerWidth > 900 &&
+                <p className="opacity-0 dp:opacity-0 group-hover:opacity-100 invisible dp:invisible group-hover:visible absolute right-0 mr-[-180px] top-1/2 transform -translate-y-1/2 bg-indigo-500 text-indigo-50 rounded px-2 py-1 text-sm transition duration-300 ease-in-out">
+                  Trykk {index + 1} på tastaturet
+                </p>
+              }
             </div>
           ))}
         </div>
       </div>
-      <div className="flex space-x-4 mt-10">
-        <button onClick={prevQuestion} disabled={currentQuestion === 0} className="tracking-wider rounded-md bg-violet-500 px-5 py-3 text-xl font-semibold text-indigo-50 shadow-sm hover:bg-violet-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-900">Tilbake</button>
+      <div className="space-x-4 mt-5">
+        <button 
+          onClick={prevQuestion} 
+          disabled={currentQuestion === 0} 
+          className="group relative cursor-pointer tracking-wider rounded-md bg-violet-500 px-5 py-3 text-xl font-semibold text-indigo-50 shadow-sm hover:bg-violet-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-900"
+        >
+          Tilbake
+          {window.innerWidth > 900 &&
+            <p className="font-normal whitespace-nowrap opacity-0 dp:opacity-0 group-hover:opacity-100 invisible dp:invisible group-hover:visible absolute left-0 ml-[50px] top-20 transform -translate-y-1/2 bg-indigo-500 text-indigo-50 rounded px-2 py-1 text-sm transition duration-300 ease-in-out">
+              Trykk venstre pil
+            </p>
+          }
+        </button>
         <button 
           onClick={nextQuestion} 
           disabled={answers[currentQuestion] === null}
-          className='tracking-wider rounded-md bg-violet-500 px-5 py-3 text-xl font-semibold text-indigo-50 shadow-sm hover:bg-violet-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-900'
+          className='group relative cursor-pointer tracking-wider rounded-md bg-violet-500 px-5 py-3 text-xl font-semibold text-indigo-50 shadow-sm hover:bg-violet-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-900'
         >
           Neste
+          {window.innerWidth > 900 &&
+            <p className="font-normal whitespace-nowrap opacity-0 dp:opacity-0 group-hover:opacity-100 invisible top-20 dp:invisible group-hover:visible absolute right-0 mr-[25px] transform -translate-y-1/2 bg-indigo-500 text-indigo-50 rounded px-2 py-1 text-sm transition duration-300 ease-in-out">
+              Trykk Enter / pil høyre
+            </p>
+          }
         </button>
       </div>
     </div>
